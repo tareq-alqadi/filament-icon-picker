df5132f373ecd75d2ad4f15307ac7aee3d1f0ebb
Tareq Al-Qadi <alqadi.tareq199@gmail.com>
2024-07-01 21:29:35 +0300

    cache icons

--- src/Forms/IconPicker.php
+++ src/Forms/IconPicker.php
@@ -10,8 +10,10 @@ use Guava\FilamentIconPicker\Forms\Concerns\CanBeCacheable;
 use Guava\FilamentIconPicker\Layout;
 use Illuminate\Contracts\Support\Arrayable;
 use Illuminate\Contracts\Support\Htmlable;
+use Illuminate\Support\Benchmark;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\File;
 use Illuminate\View\View;
 use Symfony\Component\Finder\SplFileInfo;
@@ -40,8 +42,10 @@ class IconPicker extends Select
         parent::setUp();
 
         $this->sets(config('icon-picker.sets', null));
-        $this->columns(config('icon-picker.columns', 1));
+        $this->columns(6);
+        // $this->columns(config('icon-picker.columns', 1));
         $this->layout(config('icon-picker.layout', Layout::FLOATING));
+        $this->preload();
 
         $this->getSearchResultsUsing = function (IconPicker $component, string $search, Collection $icons) {
 
@@ -50,25 +54,73 @@ class IconPicker extends Select
 
             return $this->tryCache($key, function () use ($component, $search, $icons) {
                 return collect($icons)
-                    ->filter(fn(string $icon) => str_contains($icon, $search))
+                    ->filter(fn (string $icon) => str_contains($icon, $search))
+                    ->take(50)
                     ->mapWithKeys(function (string $icon) use ($component) {
-                        return [$icon => $component->getItemTemplate(['icon' => $icon])];
+                        return [$icon => $component->getItemTemplate([
+                            'icon' => $icon,
+                            // 'iconName' => $iconName,
+                        ])];
                     })
                     ->toArray();
             });
         };
 
+        $this->options(function (IconPicker $component, Collection $icons) {
+            return collect($icons)
+                ->take(50)
+                ->mapWithKeys(function (string $icon) use ($component) {
+                    return [$icon => $component->getItemTemplate([
+                        'icon' => $icon,
+                        // 'iconName' => $iconName,
+                    ])];
+                })
+                ->toArray();
+        });
+
         $this->getOptionLabelUsing = function (IconPicker $component, $value) {
             if ($value) {
-                return $component->getItemTemplate(['icon' => $value]);
+
+                return $component->getItemTemplate([
+                    // 'icon' => svg($value),
+                    'icon' => $value
+                ]);
             }
         };
 
         $this
             ->itemTemplate(function (IconPicker $component, string $icon) {
-                return \view('filament-icon-picker::item', [
-                    'icon' => $icon,
-                ])->render();
+                $key = "icon-picker.template.$icon";
+                return $this->tryCache($key, function () use ($icon) {
+
+                    // return <<<Html
+                    //             <div class="flex justify-start items-center gap-2">
+                    //                 <div class="w-10 h-10 p-2 border border-gary-200 dark:border-gray-700 rounded-lg flex justify-center items-center">
+                    //                     {$icon}
+                    //                 </div>
+                    //                 <div class="flex flex-col gap-1">
+                    //                     <h1>{$iconName}</h1>
+                    //                     <p class="text-gray-600 dark:text-gray-400">{$iconName}</p>
+                    //                 </div>
+                    //             </div>
+                    //         Html;
+
+                    // return <<<Html
+                    //     <div class="flex flex-col items-center justify-center">
+                    //         <div class="relative w-full !h-16 flex flex-col items-center justify-center py-2">
+                    //             <div class="relative w-12 h-12 grow-1 shrink-0 gap-1">
+                    //                 $icon
+                    //                 <div class="w-full h-full absolute z-10"></div>
+                    //             </div>
+                    //             <small class="w-full text-center grow-0 shrink-0 h-4 truncate">$iconName</small>
+                    //         </div>
+                    //     </div>
+                    // Html;
+
+                    return view('filament-icon-picker::item', [
+                        'icon' => $icon,
+                    ])->render();
+                });
             })
             ->placeholder(function () {
                 return $this->view('filament-icon-picker::placeholder')->render();
@@ -163,15 +215,22 @@ class IconPicker extends Select
         return $results;
     }
 
+    public function getOptions(): array
+    {
+        return $this->evaluate($this->options, [
+            'icons' => $this->loadIcons(),
+        ]);
+    }
+
     public function relationship(string|Closure|null $name = null, string|Closure|null $titleAttribute = null, ?Closure $modifyQueryUsing = null, bool $ignoreRecord = false): static
     {
         throw new \BadMethodCallException('Method not allowed.');
     }
 
-    public function options(Arrayable|Closure|array|string|null $options): static
-    {
-        throw new \BadMethodCallException('Method not allowed.');
-    }
+    // public function options(Arrayable|Closure|array|string|null $options): static
+    // {
+    //     throw new \BadMethodCallException('Method not allowed.');
+    // }
 
     public function allowHtml(bool|Closure $condition = true): static
     {
@@ -220,6 +279,7 @@ class IconPicker extends Select
 
     private function loadIcons(): Collection
     {
+
         $iconsHash = md5(serialize($this->getSets()));
         $key = "icon-picker.fields.{$iconsHash}.{$this->getStatePath()}";
 
@@ -234,38 +294,50 @@ class IconPicker extends Select
                 $sets = collect($iconsFactory->all());
 
                 if ($allowedSets) {
-                    $sets = $sets->filter(fn($value, $key) => in_array($key, $allowedSets));
+                    $sets = $sets->filter(fn ($value, $key) => in_array($key, $allowedSets));
                 }
 
                 return [$sets, $allowedIcons, $disallowedIcons];
-            });
-
-        $icons = [];
-
-        foreach ($sets as $set) {
-            $prefix = $set['prefix'];
-            foreach ($set['paths'] as $path) {
-                // To include icons from sub-folders, we use File::allFiles instead of File::files
-                // See https://github.com/blade-ui-kit/blade-icons/blob/ce60487deeb7bcbccd5e69188dc91b4c29622aff/src/IconsManifest.php#L40
-                foreach (File::allFiles($path) as $file) {
-                    // Simply ignore files that aren't SVGs
-                    if ($file->getExtension() !== 'svg') {
-                        continue;
-                    }
-
-                    $iconName = $this->getIconName($file, parentPath: $path, prefix: $prefix);
-
-                    if ($allowedIcons && !in_array($iconName, $allowedIcons)) {
-                        continue;
-                    }
-                    if ($disallowedIcons && in_array($iconName, $disallowedIcons)) {
-                        continue;
-                    }
-
-                    $icons[] = $iconName;
-                }
             }
-        }
+        );
+
+        $iconsKey = "icon-picker.icons.{$iconsHash}.{$this->getStatePath()}";
+
+        $icons = $this->tryCache(
+            $iconsKey,
+            function () use ($sets, $allowedIcons, $disallowedIcons) {
+                $icons = [];
+
+                foreach ($sets as $set) {
+                    $prefix = $set['prefix'];
+                    foreach ($set['paths'] as $path) {
+                        // To include icons from sub-folders, we use File::allFiles instead of File::files
+                        // See https://github.com/blade-ui-kit/blade-icons/blob/ce60487deeb7bcbccd5e69188dc91b4c29622aff/src/IconsManifest.php#L40
+                        foreach (File::allFiles($path) as $file) {
+                            // Simply ignore files that aren't SVGs
+                            if ($file->getExtension() !== 'svg') {
+                                continue;
+                            }
+
+                            $iconName = $this->getIconName($file, parentPath: $path, prefix: $prefix);
+
+                            if ($allowedIcons && !in_array($iconName, $allowedIcons)) {
+                                continue;
+                            }
+                            if ($disallowedIcons && in_array($iconName, $disallowedIcons)) {
+                                continue;
+                            }
+
+                            // $icons[$iconName] = File::get($file->getRealPath());
+                            $icons[] = $iconName;
+                        }
+                    }
+                }
+
+
+                return $icons;
+            }
+        );
 
         return collect($icons);
     }
@@ -274,7 +346,8 @@ class IconPicker extends Select
      * @see https://github.com/blade-ui-kit/blade-icons and its IconsManifest.php
      * @see https://github.com/blade-ui-kit/blade-icons/blob/ce60487deeb7bcbccd5e69188dc91b4c29622aff/src/IconsManifest.php#L78
      */
-    private function getIconName(SplFileInfo $file, string $parentPath, string $prefix): string {
+    private function getIconName(SplFileInfo $file, string $parentPath, string $prefix): string
+    {
         // BladeIcons uses a simple (and view-compliant) naming convention for icon names
         // `xtra-icon` is the `icon.svg` from the `xtra` icon set
         // `xtra-dir.icon` is the `icon.svg` from the `dir/` folder from the `xtra` icon set
