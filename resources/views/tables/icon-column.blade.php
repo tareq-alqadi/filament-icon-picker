ff1277388ed666704a0a57c6440177265d64dd43
Tareq Al-Qadi <alqadi.tareq199@gmail.com>
2024-07-01 23:25:46 +0300

    Add filament features to icon column

--- resources/views/tables/icon-column.blade.php
+++ resources/views/tables/icon-column.blade.php
@@ -1,5 +1,42 @@
-<div class="filament-icon-picker-icon-column px-4 py-3">
-	@if($icon = $getState())
-		<x-icon class="h-6" name="{{$icon}}" />
-	@endif
+@php
+    use Filament\Tables\Columns\IconColumn\IconColumnSize;
+@endphp
+
+<div
+    {{ $attributes->merge($getExtraAttributes(), escape: false)
+	->class(['filament-icon-picker-icon-column', 'px-3 py-4' => !$isInline()]) }}>
+
+    @if ($icon = $getState())
+        @php
+            $color = $getColor($state) ?? 'gray';
+            $size = $getSize($state) ?? IconColumnSize::Large;
+        @endphp
+
+        <x-filament::icon :name="$icon" @class([
+            'fi-ta-icon-item',
+            match ($size) {
+                IconColumnSize::ExtraSmall, 'xs' => 'fi-ta-icon-item-size-xs h-3 w-3',
+                IconColumnSize::Small, 'sm' => 'fi-ta-icon-item-size-sm h-4 w-4',
+                IconColumnSize::Medium, 'md' => 'fi-ta-icon-item-size-md h-5 w-5',
+                IconColumnSize::Large, 'lg' => 'fi-ta-icon-item-size-lg h-6 w-6',
+                IconColumnSize::ExtraLarge, 'xl' => 'fi-ta-icon-item-size-xl h-7 w-7',
+                IconColumnSize::TwoExtraLarge,
+                IconColumnSize::ExtraExtraLarge,
+                '2xl'
+                    => 'fi-ta-icon-item-size-2xl h-8 w-8',
+                default => $size,
+            },
+            match ($color) {
+                'gray' => 'text-gray-400 dark:text-gray-500',
+                default => 'fi-color-custom text-custom-500 dark:text-custom-400',
+            },
+            is_string($color) ? 'fi-color-' . $color : null,
+        ]) @style([
+            \Filament\Support\get_color_css_variables($color, shades: [400, 500], alias: 'tables::columns.icon-column.item') => $color !== 'gray',
+        ]) />
+    @elseif (($placeholder = $getPlaceholder()) !== null)
+        <x-filament-tables::columns.placeholder>
+            {{ $placeholder }}
+        </x-filament-tables::columns.placeholder>
+    @endif
 </div>
